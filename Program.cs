6ae3dc9a5097859fa25a952fbf16d325acc99a34
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-05-11 15:57:02 +0900

    add

--- Program.cs
+++ Program.cs
@@ -9,5 +9,11 @@ class Program
 
         // aFriend変数を使用して何か処理を実行
         Console.WriteLine($"Hello, {aFriend}!");
+
+        Console.WriteLine("Hello " + aFriend);
+        Console.WriteLine($"Hello {aFriend}");
+        string firstFriend = "NaNa";
+        string secondFriend = "RUBY";
+        Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
     }
 }
